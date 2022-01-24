c94abb6d3a763ba57e61bbe27c4c976271c1186d
gribdesbois <gribdesbois@gmail.com>
2022-01-24 12:13:13 +0100

    NavMenu

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react'
 import { useResponse } from '@curi/react-dom'
 import logo from './logo.svg'
 import './App.css'
+import NavMenu from './components/NavMenu'
 
 function App() {
   const { response } = useResponse()
   const { body: Body } = response
-  return <Body response={response} />
+  return (
+    <>
+      <header>
+        <NavMenu />
+      </header>
+      <main>
+        <Body response={response} />
+      </main>
+    </>
+  )
 }
 
 export default App
